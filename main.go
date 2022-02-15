74195394f7e6c3145ac70842a1b7bde4d06f028c
Kevin Mulvey <kmulvey@linux.com>
2022-02-14 17:13:00 -0700

    clean

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/kmulvey/go-kv-bench/benchbadger"
 )
 
-/*
-func init() {
-	var err = os.Mkdir("testdata", 0755)
-	fmt.Println("err:", err)
-	if err != nil {
-		panic(err)
-	}
-}
-*/
-
 func main() {
-	res := testing.Benchmark(benchbadger.BenchmarkBadgerDBPutValue64B)
+	res := testing.Benchmark(benchbadger.BenchmarkBadgerDBPutValue512B)
 	fmt.Printf("%s\n%#[1]v\n", res)
+	os.RemoveAll("benchdata")
 }
