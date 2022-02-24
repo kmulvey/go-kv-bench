e6e39473ca34c3aae5e4610437ddfd13ddec903e
Kevin Mulvey <kmulvey@linux.com>
2022-02-23 18:14:30 -0700

    gh pages

---
+++ config.js
@@ -0,0 +1,45 @@
+const COLORS = [
+    '#4dc9f6',
+    '#f67019',
+    '#f53794',
+    '#537bc4',
+    '#acc236',
+    '#166a8f',
+    '#00a950',
+    '#58595b',
+    '#8549ba'
+];
+
+function createChartConfig(title, benchdata) {
+    return {
+        type: 'line',
+        data: benchdata,
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: title
+                }
+            },
+            scales: {
+                x: {
+                    type: 'time',
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: title
+                    }
+                },
+            }
+        },
+    }
+}
